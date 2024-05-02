e6440d110a7085fb1c33b77678f899c775cdd166
CrazyCatZhang <crazycatzhang@outlook.com>
2024-05-02 23:32:27 +0800

    ✨ feat: bubble/insert/selection sort

---
+++ sort/insertSort.js
@@ -0,0 +1,17 @@
+function insertSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+
+// test
+let arr = [5, 3, 4, 1, 2];
+insertSort(arr);
+console.log(arr); // [1, 2, 3, 4, 5]
---
+++ sort/selectionSort.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+}
+
+// test
+let arr = [5, 3, 4, 1, 2];
+selectionSort(arr);
+console.log(arr); // [1, 2, 3, 4, 5]
