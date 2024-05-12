ee19152e7678a1d8042373a8a96fc79843ac2477
CrazyCatZhang <crazycatzhang@outlook.com>
2024-05-12 17:44:24 +0800

    ✨ feat: mergeSort

---
+++ sort/mergeSort.js
@@ -0,0 +1,36 @@
+function mergeSort(arr) {
+  if (!arr || arr.length === 0) {
+    return [];
+  }
+
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length >> 1);
+  const leftSort = mergeSort(arr.slice(0, mid));
+  const rightSort = mergeSort(arr.slice(mid));
+  return merge(leftSort, rightSort);
+}
+
+function merge(left, right) {
+  const res = [];
+  let i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      res.push(left[i++]);
+    } else {
+      res.push(right[j++]);
+    }
+  }
+
+  return res.concat(left.slice(i)).concat(right.slice(j));
+}
+
+// Test
+// Give me a random array
+let arr = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100));
+console.log(arr);
+console.log(mergeSort(arr));
