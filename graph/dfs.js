44b5f979f3d2b3f384a5fe357eae799a0133d3a3
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-04 21:22:16 +0800

    ✨ feat: graph dfs

---
+++ graph/dfs.js
@@ -0,0 +1,20 @@
+function dfs(node) {
+  if (node === null) return;
+  const stack = [];
+  const set = new Set();
+  stack.push(node);
+  set.add(node);
+  console.log(node.value);
+  while (stack.length) {
+    const cur = stack.pop();
+    for (let next of cur.nexts) {
+      if (!set.has(next)) {
+        stack.push(cur);
+        stack.push(next);
+        set.add(next);
+        console.log(next.value);
+        break;
+      }
+    }
+  }
+}
