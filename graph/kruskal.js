5c94e5fbe04cdf666db4f34c66572727fe39f0a2
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-06 00:10:33 +0800

    ✨ feat: add kruskal

---
+++ graph/kruskal.js
@@ -0,0 +1,16 @@
+const UnionFind = require("./union-find");
+
+function kruskal(graph) {
+  const unionFind = new UnionFind(graph.nodes.values());
+  const edges = Array.from(graph.edges).sort((a, b) => a.weight - b.weight);
+  const result = [];
+  for (const edge of edges) {
+    if (!unionFind.isSameSet(edge.from, edge.to)) {
+      result.push(edge);
+      unionFind.union(edge.from, edge.to);
+    }
+  }
+  return result;
+}
+
+module.exports = kruskal;
