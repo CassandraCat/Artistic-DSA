d1fa0715715a6fb12f04655dea95233a60d26014
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-10 20:07:08 +0800

    ✨ feat: add lowest string

---
+++ sort/lowestLexicography.js
@@ -0,0 +1,13 @@
+function lowestString(strs) {
+  if (strs === null || strs.length === 0) {
+    return "";
+  }
+
+  strs = strs.sort((a, b) => a + b <= b + a);
+  let result = "";
+  for (let i = 0; i < strs.length; i++) {
+    result += strs[i];
+  }
+
+  return result;
+}
