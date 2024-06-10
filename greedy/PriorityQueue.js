89aafbb4801eef837759dc44a9bde07f7db3b49b
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-10 20:48:07 +0800

    ✨ feat: add find maximized capital

---
+++ greedy/PriorityQueue.js
@@ -0,0 +1,89 @@
+class PriorityQueue {
+  constructor(comparator = (a, b) => a - b) {
+    this.heap = [];
+    this.comparator = comparator;
+  }
+
+  enqueue(element) {
+    this.heap.push(element);
+    this.bubbleUp();
+  }
+
+  dequeue() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown(0);
+
+    return root;
+  }
+
+  peek() {
+    return this.heap.length > 0 ? this.heap[0] : null;
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+    const element = this.heap[index];
+
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parent = this.heap[parentIndex];
+
+      if (this.comparator(element, parent) >= 0) break;
+
+      this.heap[index] = parent;
+      index = parentIndex;
+    }
+
+    this.heap[index] = element;
+  }
+
+  bubbleDown(index) {
+    const length = this.heap.length;
+    const element = this.heap[index];
+
+    while (true) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.heap[leftChildIndex];
+        if (this.comparator(leftChild, element) < 0) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.heap[rightChildIndex];
+        if (
+          (swap === null && this.comparator(rightChild, element) < 0) ||
+          (swap !== null && this.comparator(rightChild, leftChild) < 0)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+
+      this.heap[index] = this.heap[swap];
+      index = swap;
+    }
+
+    this.heap[index] = element;
+  }
+}
+
+module.exports = PriorityQueue;
---
+++ greedy/ipo.js
@@ -0,0 +1,38 @@
+const PriorityQueue = require("./PriorityQueue");
+
+class Node {
+  constructor(profit, capital) {
+    this.profit = profit;
+    this.capital = capital;
+  }
+}
+
+function findMaximizedCapital(k, W, profits, capital) {
+  const minCostQ = new PriorityQueue((a, b) => a.capital - b.capital);
+  const maxProfitQ = new PriorityQueue((a, b) => b.profit - a.profit);
+
+  for (let i = 0; i < profits.length; i++) {
+    minCostQ.enqueue(new Node(profits[i], capital[i]));
+  }
+
+  for (let i = 0; i < k; i++) {
+    while (!minCostQ.isEmpty() && minCostQ.peek().capital <= W) {
+      maxProfitQ.enqueue(minCostQ.dequeue());
+    }
+
+    if (maxProfitQ.isEmpty()) {
+      break;
+    }
+
+    W += maxProfitQ.dequeue().profit;
+  }
+
+  return W;
+}
+
+// Test
+const profits = [1, 2, 3];
+const capital = [0, 1, 1];
+const k = 2;
+const W = 0;
+console.log(findMaximizedCapital(k, W, profits, capital)); // 4
