5a2e46d044b5a19c3622319529a317e87d742b86
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-16 10:58:20 +0800

    ✨ feat: add print sub string

---
+++ recursive/printSubStr.js
@@ -0,0 +1,16 @@
+function printSubString(charArr, index, res) {
+  if (index === charArr.length) {
+    console.log(res);
+    return;
+  }
+
+  printSubString(charArr, index + 1, res + charArr[index]);
+  printSubString(charArr, index + 1, res);
+}
+
+function test() {
+  const charArr = ["a", "b", "c"];
+  printSubString(charArr, 0, "");
+}
+
+test();
