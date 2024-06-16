838de366d1f1de078f8df304df1d6b99ff83ef94
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-16 16:34:41 +0800

    ✨ feat: add reverse stack

---
+++ recursive/reverseStack.js
@@ -0,0 +1,25 @@
+function reverse(stack) {
+  if (stack.length === 0) {
+    return stack;
+  }
+
+  const last = process(stack);
+  reverse(stack);
+  stack.push(last);
+}
+
+function process(stack) {
+  const result = stack.pop();
+  if (stack.length === 0) {
+    return result;
+  }
+
+  const last = process(stack);
+  stack.push(result);
+  return last;
+}
+
+// Test
+const stack = [1, 2, 3, 4, 5];
+reverse(stack);
+console.log(stack); // [5, 4, 3, 2, 1]
