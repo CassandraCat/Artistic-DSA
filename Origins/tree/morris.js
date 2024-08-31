166567e4ce855e5e1dd414abd9088abd123a8e85
CassandraCat <CassandraCat@proton.me>
2024-08-31 11:07:48 +0800

    ✨ feat: pre in post order traversal with morris

--- Origins/tree/morris.js
+++ Origins/tree/morris.js
@@ -39,3 +39,114 @@ function morris(head) {
         current = current.right;
     }
 }
+
+function morrisPreOrder(head) {
+    if (!head) {
+        return;
+    }
+
+    let current = head;
+    let mostRight = null;
+
+    while (current) {
+        mostRight = current.left;
+        if (mostRight) {
+            while (mostRight.right && mostRight.right !== current) {
+                mostRight = mostRight.right;
+            }
+
+            if (!mostRight.right) {
+                console.log(current.value + " ");
+                mostRight.right = current;
+                current = current.left;
+                continue;
+            } else {
+                mostRight.right = null;
+            }
+        } else {
+            console.log(current.value + " ");
+        }
+        current = current.right;
+    }
+}
+
+function morrisInOrder(head) {
+    if (!head) {
+        return;
+    }
+
+    let current = head;
+    let mostRight = null;
+
+    while (current) {
+        mostRight = current.left;
+        if (mostRight) {
+            while (mostRight.right && mostRight.right !== current) {
+                mostRight = mostRight.right;
+            }
+
+            if (!mostRight.right) {
+                mostRight.right = current;
+                current = current.left;
+                continue;
+            } else {
+                mostRight.right = null;
+            }
+        }
+        console.log(current.value + " ");
+        current = current.right;
+    }
+}
+
+function reverseNode(from) {
+    let pre = null;
+    let next = null;
+
+    while (from) {
+        next = from.right;
+        from.right = pre;
+        pre = from;
+        from = next;
+    }
+
+    return pre;
+}
+
+function printList(head) {
+    let tail = reverseNode(head);
+    let current = tail;
+    while (current) {
+        console.log(current.value + " ");
+        current = current.right;
+    }
+    reverseNode(tail);
+}
+
+function morrisPostOrder(head) {
+    if (!head) {
+        return;
+    }
+
+    let current = head;
+    let mostRight = null;
+
+    while (current) {
+        mostRight = current.left;
+        if (mostRight) {
+            while (mostRight.right && mostRight.right !== current) {
+                mostRight = mostRight.right;
+            }
+
+            if (!mostRight.right) {
+                mostRight.right = current;
+                current = current.left;
+                continue;
+            } else {
+                mostRight.right = null;
+                printList(current.left);
+            }
+        }
+        current = current.right;
+    }
+    printList(head);
+}
